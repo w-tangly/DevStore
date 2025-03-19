1666944f876d27c675da9b0df277d59d349b8cc8
pedrh-carmo <pedro.campos.1bmangal@gmail.com>
2025-03-19 15:57:06 -0300

    Final - Menu burg

---
+++ assets/js/header.js
@@ -0,0 +1,14 @@
+// Menu hamburguer
+let menuBurger = document.querySelector('.menu-burger')
+let headerMenu = document.querySelector('.header-menu')
+
+// Evento click para mostrar e esconder o menu hamburguer
+menuBurger.addEventListener('click', ()=>{
+    if(headerMenu.style.display==='block'){
+        headerMenu.style.display='none'
+        menuBurger.classList.remove('active')
+    }else{
+        headerMenu.style.display='block'
+        menuBurger.classList.add('active')
+    }
+})  
