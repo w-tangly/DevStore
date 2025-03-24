44720626adae0c1bbb3d373e5bde917fe0f96333
pedrh-carmo <pedro.campos.1bmangal@gmail.com>
2025-03-24 15:57:17 -0300

    Fim de segunda (pós pagamento camiseta)

---
+++ db.php
@@ -0,0 +1,20 @@
+<?php
+    // variável para o servidor do banco
+    $host = "localhost";
+
+    // variável para o usuário de acesso ao banco
+    $user = "root";
+
+    // variável para a senha do usuário
+    $password = "";
+
+    // variável para o nome do banco
+    $dbname = "devstore";
+
+    // tenta conectar ao banco
+    try{
+        $pdo = new PDO("mysql:host=$host; dbname=$dbname; charset=utf-8;", $user, $password);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }catch (PDOException $e){
+        die("Falha na conexão: ". $e->getMessage());
+    }
